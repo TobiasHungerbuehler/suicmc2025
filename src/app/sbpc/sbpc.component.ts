5005abb76ad37d7296210fe03030b42074ad5b73
Tobias Hungerbühler <to.hungerbuehler@gmail.com>
2024-09-12 12:35:12 +0200

    pfade

---
+++ src/app/sbpc/sbpc.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sbpc',
+  standalone: true,
+  imports: [],
+  templateUrl: './sbpc.component.html',
+  styleUrl: './sbpc.component.scss'
+})
+export class SbpcComponent {
+
+}
---
+++ src/app/suicmc/suicmc.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-suicmc',
+  standalone: true,
+  imports: [],
+  templateUrl: './suicmc.component.html',
+  styleUrl: './suicmc.component.scss'
+})
+export class SuicmcComponent {
+
+}
