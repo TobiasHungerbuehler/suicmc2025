0e32ea70f52e5b4574c0bf66e12c4c3d541cb30c
Tobias Hungerbühler <to.hungerbuehler@gmail.com>
2024-09-13 21:36:36 +0200

    start done

--- src/app/sbpc/sbpc.component.ts
+++ src/app/sbpc/sbpc.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { StartComponent } from '../shared/start/start.component';
 
 @Component({
   selector: 'app-sbpc',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, StartComponent],
   templateUrl: './sbpc.component.html',
   styleUrl: './sbpc.component.scss'
 })
