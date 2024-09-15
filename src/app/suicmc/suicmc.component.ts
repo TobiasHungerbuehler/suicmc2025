770e167131b801cf7d1560910a65c298c57c05fc
Tobias Hungerbühler <to.hungerbuehler@gmail.com>
2024-09-15 16:53:17 +0200

    unser verein

--- src/app/suicmc/suicmc.component.ts
+++ src/app/suicmc/suicmc.component.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { StartComponent } from '../shared/start/start.component';
+import { UnserVereinComponent } from '../shared/unser-verein/unser-verein.component';
+import { AnmeldungComponent } from '../shared/anmeldung/anmeldung.component';
 
 @Component({
   selector: 'app-suicmc',
   standalone: true,
-  imports: [CommonModule, StartComponent],
+  imports: [CommonModule, StartComponent, AnmeldungComponent, UnserVereinComponent],
   templateUrl: './suicmc.component.html',
   styleUrl: './suicmc.component.scss'
 })
