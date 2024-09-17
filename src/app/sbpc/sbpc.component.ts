0dbbd1220cc9da673462a8bdd30a5c62669187ba
Tobias Hungerbühler <to.hungerbuehler@gmail.com>
2024-09-17 08:33:59 +0200

    footer v1

--- src/app/sbpc/sbpc.component.ts
+++ src/app/sbpc/sbpc.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { StartComponent } from '../shared/start/start.component';
 import { UnserVereinComponent } from '../shared/unser-verein/unser-verein.component';
 import { AnmeldungComponent } from '../shared/anmeldung/anmeldung.component';
+import { FooterComponent } from '../shared/footer/footer.component';
 
 @Component({
   selector: 'app-sbpc',
   standalone: true,
-  imports: [CommonModule, StartComponent, AnmeldungComponent,UnserVereinComponent],
+  imports: [CommonModule, StartComponent, AnmeldungComponent,UnserVereinComponent, FooterComponent],
   templateUrl: './sbpc.component.html',
   styleUrl: './sbpc.component.scss'
 })
--- src/app/suicmc/suicmc.component.ts
+++ src/app/suicmc/suicmc.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { StartComponent } from '../shared/start/start.component';
 import { UnserVereinComponent } from '../shared/unser-verein/unser-verein.component';
 import { AnmeldungComponent } from '../shared/anmeldung/anmeldung.component';
+import { FooterComponent } from '../shared/footer/footer.component';
 
 @Component({
   selector: 'app-suicmc',
   standalone: true,
-  imports: [CommonModule, StartComponent, AnmeldungComponent, UnserVereinComponent],
+  imports: [CommonModule, StartComponent, AnmeldungComponent, UnserVereinComponent, FooterComponent],
   templateUrl: './suicmc.component.html',
   styleUrl: './suicmc.component.scss'
 })
