822644aeb7264f0e58e7a61c0815df5b605df85a
TobiasHungerbuehler <to.hungerbuehler@gmail.com>
2025-01-17 08:01:44 +0100

    init

--- src/app/shared/verhalten/verhalten.component.ts
+++ src/app/shared/verhalten/verhalten.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+import { MatExpansionModule } from "@angular/material/expansion";
 
 @Component({
-  selector: 'app-verhalten',
-  standalone: true,
-  imports: [],
-  templateUrl: './verhalten.component.html',
-  styleUrl: './verhalten.component.scss'
+    selector: "app-verhalten",
+    standalone: true,
+    imports: [CommonModule, MatExpansionModule],
+    templateUrl: "./verhalten.component.html",
+    styleUrl: "./verhalten.component.scss",
 })
-export class VerhaltenComponent {
-
-}
+export class VerhaltenComponent {}
