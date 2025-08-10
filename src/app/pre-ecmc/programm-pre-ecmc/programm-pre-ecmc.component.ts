0aab469804605591b82376e4d15ea0546a6f0fa1
Tobias Hungerbühler <76483987+TobiasHungerbuehler@users.noreply.github.com>
2025-08-10 17:47:35 +0200

    Update event programs

--- src/app/pre-ecmc/programm-pre-ecmc/programm-pre-ecmc.component.ts
+++ src/app/pre-ecmc/programm-pre-ecmc/programm-pre-ecmc.component.ts
@@ -42,28 +42,30 @@ export class ProgrammPreEcmcComponent {
     text = [
         {
             text: {
-                de: `Genaue Öffnungszeiten der Registration folgen. Die Registration ist jedoch an allen Standorten für Teilnehmende und Begleitpersonen möglich.`,
-                en: `Exact opening hours of the registration will follow. However, registration is available at all locations for participants and accompanying persons.`,
+                de: `Programmänderungen vorbehalten.`,
+                en: `Schedule subject to change.`,
             },
         },
     ];
 
     rulesText = [
         {
-            dayLabel: { de: "SONNTAG", en: "SUNDAY" },
-            dateLabel: { de: "Sonntag, 07. 09. 2025", en: "Sunday, 07. 09. 2025" },
+            dayLabel: { de: 'SONNTAG', en: 'SUNDAY' },
+            dateLabel: { de: 'Sonntag, 07. 09. 2025', en: 'Sunday, 07. 09. 2025' },
             text: {
-                de: `15 – 20 Uhr Welcome Drink @Theater
-17 – 20 Uhr Essen @Theater
-20 – 23 Uhr Party @Engel`,
-                en: `15 – 20 Uhr Welcome Drink @Theater
-17 – 20 Uhr Food @Theater
-20 – 23 Uhr Party @Engel`,
+                de: `15 – 20 Uhr Welcome Drink @Areal Theater
+17 – 20 Uhr Essen @Areal Theater
+20 – 23 Uhr Registration/Infopoint @Schwarzer Engel
+20 – 23 Uhr Party @Schwarzer Engel`,
+                en: `15 – 20 Uhr Welcome Drink @Areal Theater
+17 – 20 Uhr Food @Areal Theater
+20 – 23 Uhr Registration @Schwarzer Engel
+20 – 23 Uhr Party @Schwarzer Engel`,
             },
         },
         {
-            dayLabel: { de: "MONTAG", en: "MONDAY" },
-            dateLabel: { de: "Montag, 08. 09. 2025", en: "Monday, 08. 09. 2025" },
+            dayLabel: { de: 'MONTAG', en: 'MONDAY' },
+            dateLabel: { de: 'Montag, 08. 09. 2025', en: 'Monday, 08. 09. 2025' },
             text: {
                 de: `08 – 10 Uhr Frühstück @Kreuzbleiche
 10 Uhr Start Group-Ride @Kreuzbleiche`,
