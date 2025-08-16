355e4702f7eb248704e22aaa69e6f9aaf0d0002e
Tobias Hungerbühler <76483987+TobiasHungerbuehler@users.noreply.github.com>
2025-08-16 21:02:08 +0200

    Update program schedules

--- src/app/pre-ecmc/programm-pre-ecmc/programm-pre-ecmc.component.ts
+++ src/app/pre-ecmc/programm-pre-ecmc/programm-pre-ecmc.component.ts
@@ -53,14 +53,14 @@ export class ProgrammPreEcmcComponent {
             dayLabel: { de: 'SONNTAG', en: 'SUNDAY' },
             dateLabel: { de: 'Sonntag, 07. 09. 2025', en: 'Sunday, 07. 09. 2025' },
             text: {
-                de: `15 – 20 Uhr Welcome Drink @Areal Theater
+                de: `15 – 20 Uhr Get together @Areal Theater
 17 – 20 Uhr Essen @Areal Theater
-20 – 23 Uhr Registration/Infopoint @Schwarzer Engel
-20 – 23 Uhr Party @Schwarzer Engel`,
-                en: `15 – 20 Uhr Welcome Drink @Areal Theater
+20 – 23 Uhr Party @Schwarzer Engel
+20 – 23 Uhr Registration @Schwarzer Engel`,
+                en: `15 – 20 Uhr Get together @Areal Theater
 17 – 20 Uhr Food @Areal Theater
-20 – 23 Uhr Registration @Schwarzer Engel
-20 – 23 Uhr Party @Schwarzer Engel`,
+20 – 23 Uhr Party @Schwarzer Engel
+20 – 23 Uhr Registration @Schwarzer Engel`,
             },
         },
         {
@@ -68,9 +68,9 @@ export class ProgrammPreEcmcComponent {
             dateLabel: { de: 'Montag, 08. 09. 2025', en: 'Monday, 08. 09. 2025' },
             text: {
                 de: `08 – 10 Uhr Frühstück @Kreuzbleiche
-10 Uhr Start Group-Ride @Kreuzbleiche`,
+10 Uhr Start Groupride @Kreuzbleiche`,
                 en: `08 – 10 Uhr Breakfast @Kreuzbleiche
-10 Uhr Start Group-Ride @Kreuzbleiche`,
+10 Uhr Start Groupride @Kreuzbleiche`,
             },
         },
     ];
